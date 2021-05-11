30416a7aa2e0af4bd2a3da670f702e39b358f66f
DanielFL1988 <esemierce@gmail.com>
2021-05-11 17:41:47 +0200

    delta teoretycznie działa

---
+++ delta.ts
@@ -0,0 +1,11 @@
+var wspolczynnikA: number = 1;
+var wspolczynnikB: number = 4;
+var wspolczynnikC: number = 1;
+
+console.log('Twój współczynnik a wynosi: ' + `${wspolczynnikA}`);
+console.log('Twój współczynnik b wynosi: ' + `${wspolczynnikB}`);
+console.log('Twój współczynnik c wynosi: ' + `${wspolczynnikC}`);
+
+var delta: number = (wspolczynnikB*wspolczynnikB - 4 * wspolczynnikA * wspolczynnikC);
+
+console.log('Delta wynosi: ' + `${delta}`);
