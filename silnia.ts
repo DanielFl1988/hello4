467515a58b89acfa3a1cd80016a949125333b73e
DanielFL1988 <esemierce@gmail.com>
2021-05-18 19:43:14 +0200

    poprawki do silni

--- silnia.ts
+++ silnia.ts
@@ -1,20 +1,20 @@
-let liczba: number = 2;
+//let liczba: number = 2;
 
 let silnia: number = 1;
-let liczba2: number = 5;//tutaj wpisuje z jakiej liczby chce silnie
+let liczba2: number = 3;//tutaj wpisuje z jakiej liczby chce silnie
 
-if (liczba === 0 || liczba === 1){
-    console.log(`1`);
-}
+//if (liczba === 0 || liczba === 1) {
+ //   console.log(`jeden`);
+//}
 
-if (liczba >= 2)
+//if (liczba >= 2)
     //for (let i =1; i <= liczba-1; i+=1){
-     while (liczba2 >1){
+    while (liczba2 > 1) {
         silnia *= liczba2
-       liczba2 = liczba2-1;
-       
-     } 
-   
-    
-    //}
-    console.log(silnia);
+        liczba2 = liczba2 - 1;
+
+    }
+
+
+//}
+console.log(silnia);
