6152902dedbd5050abaee2867267313d02f0c20a
DanielFL1988 <esemierce@gmail.com>
2021-05-19 16:11:53 +0200

    ciag z poprawkami

--- ciagfib.ts
+++ ciagfib.ts
@@ -2,27 +2,24 @@ let liczbaDo: number = 12;//tutaj wpisujesz do ilu liczb ma byc ciag
 let liczba0: number = 0;
 let liczba1: number = 1;
 
-if (liczbaDo<0){
+if (liczbaDo < 0) {
     console.log(`wpisz liczbę większą bądź równą zero`);
 }
-if (liczbaDo === 0 ){
+if (liczbaDo === 0) {
     console.log(liczbaDo);
 }
-if (liczbaDo === 1){
+if (liczbaDo === 1) {
     console.log(0);
     console.log(1);
 }
-if (liczbaDo>=2){
+if (liczbaDo >= 2) {
     console.log(liczba0);
     console.log(liczba1);
 }
-    for (let i = 1; i <= liczbaDo-1; i+=1) {
-            
-            let liczba2 = liczba0+liczba1;
-            console.log(liczba2);
-            liczba0=liczba1;
-            liczba1=liczba2;
-        }
+for (let i = 1; i <= liczbaDo - 1; i += 1) {
 
-   
-
+    let liczba2 = liczba0 + liczba1;
+    console.log(liczba2);
+    liczba0 = liczba1;
+    liczba1 = liczba2;
+}
