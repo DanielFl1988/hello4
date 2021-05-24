485d7ae1b1ad52e3480a41a291c7f0dddd7c6e30
DanielFL1988 <esemierce@gmail.com>
2021-05-24 20:35:28 +0200

    poprafffki

--- ciagfv3.ts
+++ ciagfv3.ts
@@ -1,6 +1,6 @@
 let liczbaStartowa: number = 4;// tutaj wpisujesz ile chcesz liczba z ciągu
-let liczba1: number = 1;
-let liczba2: number = 0;
+let liczba1x: number = 1;
+let liczba2x: number = 0;
 let liczba3: number;
 if (liczbaStartowa === 0){
     console.log(`zero`);
@@ -10,14 +10,14 @@ if (liczbaStartowa === 0){
 //    console.log(`1`);
 //}
 if (liczbaStartowa >=1 ){
-    console.log(liczba2);
+    console.log(liczba2x);
   //  console.log(`1`);
 
     while (liczbaStartowa >=2 ){
-        liczba3 = liczba1 + liczba2;
+        liczba3 = liczba1 + liczba2x;
         console.log(liczba3);
-        liczba1 = liczba2;
-        liczba2 = liczba3;
+        liczba1 = liczba2x;
+        liczba2x = liczba3;
         liczbaStartowa-=1;
     }
 
--- silnia.ts
+++ silnia.ts
@@ -1,19 +1,19 @@
 //let liczba: number = 2;
 let silnia: number = 1;
 let liczba2: number = 8;//tutaj wpisuje z jakiej liczby chce silnie
-const liczba1: number = liczba2
+const liczba1y: number = liczba2;
 //if (liczba === 0 || liczba === 1) {
 //   console.log(`jeden`);
 //}
 //if (liczba >= 2)
 //for (let i =1; i <= liczba-1; i+=1){
-if (liczba2 < 0) {
+if (liczba2x < 0) {
     console.log(`wpisz liczbę większą lub równą zero`);
 }
 else {
-    while (liczba2 > 1) {
-        silnia *= liczba2
-        liczba2 = liczba2 - 1;
+    while (liczba2x > 1) {
+        silnia *= liczba2x
+        liczba2x = liczba2x - 1;
     }
     console.log(`silnia z`, liczba1, `wynosi`, silnia);
 }
