55c1c331dbd4619699081c2bc643c1e44af93a48
DanielFL1988 <esemierce@gmail.com>
2021-06-01 11:33:58 +0200

    random poprawiony

--- random.ts
+++ random.ts
@@ -1,4 +1,11 @@
 let liczbalosowa: number = Math.random();
-let liczbapomonożona = liczbalosowa*10;
-let liczbazaokraglona = Math.round(liczbapomonożona);
+let liczbaJednosci = liczbalosowa*10;
+let liczbaDziesiatek: number = liczbalosowa*100
+let liczbaSetek:number = liczbalosowa*1000
+let liczbazaokraglona = Math.round(liczbaJednosci+liczbaDziesiatek+liczbaSetek);
+if (Math.random() <0,1){
+    let liczbaDziesiatek = 0;
+    let liczbaJednosci = 0;
+    let liczbaSetek = 0;
+}
 console.log(liczbazaokraglona);
